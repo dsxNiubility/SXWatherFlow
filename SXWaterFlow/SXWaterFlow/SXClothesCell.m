49606ba96208adb5041db6d81d10a8d8ee10815c
董铂然 <dantesx2012@gmail.com>
2015-03-21 21:06:04 +0800

    项目准备工作完毕
    项目准备工作完毕

---
+++ SXWaterFlow/SXWaterFlow/SXClothesCell.m
@@ -0,0 +1,18 @@
+//
+//  SXClothesCell.m
+//  SXWaterFlow
+//
+//  Created by 董 尚先 on 15/3/21.
+//  Copyright (c) 2015年 shangxianDante. All rights reserved.
+//
+
+#import "SXClothesCell.h"
+
+@implementation SXClothesCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+
+@end
---
+++ SXWaterFlow/SXWaterFlow/SXModels.h
@@ -0,0 +1,13 @@
+//
+//  SXModels.h
+//  SXWaterFlow
+//
+//  Created by 董 尚先 on 15/3/21.
+//  Copyright (c) 2015年 shangxianDante. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SXModels : NSObject
+
+@end
